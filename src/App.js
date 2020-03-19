0bae2bdd60f6c197c953227b6b8c0697c2481577
elianaroth <52683617+elianaroth@users.noreply.github.com>
2020-03-18 20:18:01 -0400

    Updated background color, and change text

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some happen!</h2>
+      <h1>Hello my name is Eliana!</h1>
+      <h2>I'm learning how to code :)</h2>
     </div>
   );
 }
