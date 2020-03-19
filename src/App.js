cf2086770e5dd9ce879a17559bc03cedb50a10aa
elianaroth <52683617+elianaroth@users.noreply.github.com>
2020-03-18 21:13:12 -0400

    Dino pic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import image from "./assets/Dino.jpg";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello my name is Eliana!</h1>
-      <h2>I'm learning how to code :)</h2>
+      <h2>I love dinos!</h2>
+      <img src={image} alt="dino" className="dino-image" />
     </div>
   );
 }
