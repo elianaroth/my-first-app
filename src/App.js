a8c65c7f2a910757dd5e005725dd2317a2e88f1a
elianaroth <52683617+elianaroth@users.noreply.github.com>
2020-03-19 10:50:22 -0400

    Changed background color and Dino picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import image from "./assets/Dino.jpg";
+import image from "./assets/Dino3.jpg";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello my name is Eliana!</h1>
+      <h1>Eliana Roth!</h1>
       <h2>I love dinos!</h2>
       <img src={image} alt="dino" className="dino-image" />
     </div>
